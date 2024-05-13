66ab3abd48f340e545b08a0d4e76aceb1fd65f58
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-05-12 19:55:56 -0600

    admin navigation route done

--- proyect-k/src/app/quiz/page.tsx
+++ proyect-k/src/app/quiz/page.tsx
@@ -7,7 +7,7 @@ import BarGraph from "@/components/Quiz/BarGraph";
 import UserButtons from "@/components/Authentication/UserButtons";
 import QuizSlider from "@/components/Authentication/QuizSlider";
 import { useRouter, usePathname } from "next/navigation";
-
+import { useEffect, useState } from "react";
 
 export default function Quiz() {
   const appRouter = useRouter();
@@ -24,54 +24,82 @@ export default function Quiz() {
 
   const goToGame = () => {
     appRouter.replace("/game");
-  }
+  };
+
+  const [admin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("admin");
+    console.log(user);
+    if (user === "true") {
+      setAdmin(true);
+    }
+  }, []);
 
   return (
     <div className="flex flex-col h-screen">
-      <Box
-        id="titlebox"
-        className="flex row mx-auto mt-10 mb-4 w-8/12 rounded-lg justify-center items-center gap-6"
-      >
-        <img src="/Rectangle 13.png" className="h-20" />
-        <Typography variant="h3" className="font-thin text-black">
-          Tigres o Rayados?
-        </Typography>
-      </Box>
-      <Box
-        id="middle box"
-        className="flex flex-column px-10 flex-start w-full h-4/12 flex-wrap"
-      >
-        <Box
-          id="ai player stats"
-          className="flex flex-row w-full h-2/12 justify-around gap-3"
-        >
-          {/* Each PlayerCard represents either a bot or the user. Bots should generate random answers while the user
-          should show historic stats */}
-          <PlayerCard />
-          <PlayerCard />
-          <PlayerCard />
-        </Box>
-        <Box id="bar graph boxy" className="flex w-full my-6">
-          {/* This graph should render the stats */}
-          <BarGraph />
-        </Box>
-        <Box id="user options" className="flex w-full flex-col">
-          {/* We should pass the answers and receive feedback from here */}
-          <UserButtons />
-          <Box id="final box" className="flex flex-row w-full justify-between">
-            <QuizSlider value={value} handleSliderChange={handleSliderChange} />
+      {admin ? (
+        <div>
+          Edit Quiz Page
+
+
+        <Button variant="contained" size="large" onClick={goToGame}> Go to Game </Button>
+
+        </div>
+      ) : (
+        <div>
+          <Box
+            id="titlebox"
+            className="flex row mx-auto mt-10 mb-4 w-8/12 rounded-lg justify-center items-center gap-6"
+          >
+            <img src="/Rectangle 13.png" className="h-20" />
+            <Typography variant="h3" className="font-thin text-black">
+              Tigres o Rayados?
+            </Typography>
+          </Box>
+          <Box
+            id="middle box"
+            className="flex flex-column px-10 flex-start w-full h-4/12 flex-wrap"
+          >
             <Box
-              id="submission box"
-              className="flex items-end w-2/12 justify-end mt-6"
+              id="ai player stats"
+              className="flex flex-row w-full h-2/12 justify-around gap-3"
             >
-              <Button variant="contained" size="large" onClick={goToGame}>
-                Submit
-              </Button>
+              {/* Each PlayerCard represents either a bot or the user. Bots should generate random answers while the user
+          should show historic stats */}
+              <PlayerCard />
+              <PlayerCard />
+              <PlayerCard />
+            </Box>
+            <Box id="bar graph boxy" className="flex w-full my-6">
+              {/* This graph should render the stats */}
+              <BarGraph />
+            </Box>
+            <Box id="user options" className="flex w-full flex-col">
+              {/* We should pass the answers and receive feedback from here */}
+              <UserButtons />
+              <Box
+                id="final box"
+                className="flex flex-row w-full justify-between"
+              >
+                <QuizSlider
+                  value={value}
+                  handleSliderChange={handleSliderChange}
+                />
+                <Box
+                  id="submission box"
+                  className="flex items-end w-2/12 justify-end mt-6"
+                >
+                  <Button variant="contained" size="large" onClick={goToGame}>
+                    Submit
+                  </Button>
+                </Box>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </Box>
-      <Box id="User Options"></Box>
+          <Box id="User Options"></Box>
+        </div>
+      )}
     </div>
   );
 }
