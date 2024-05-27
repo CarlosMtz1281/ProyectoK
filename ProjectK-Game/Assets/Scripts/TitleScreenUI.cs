6d73414ce9266767f7e6eb6f3bf1fdc9e1e3e9d0
Imanol González <imanolgzzsolis@gmail.com>
2024-05-27 17:12:44 -0600

    prototype of final game

--- ProjectK-Game/Assets/Scripts/TitleScreenUI.cs
+++ ProjectK-Game/Assets/Scripts/TitleScreenUI.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using LitJson;
+using TMPro;
 
 public class TitleScreenUI : MonoBehaviour
 {
-    [SerializeField] GameObject titleScreen;
-    [SerializeField] GameObject instructionsScreen;
-    [SerializeField] GameObject settingsScreen;
+    [SerializeField] GameObject titleScreen, instructionsScreen, settingsScreen, selectLevelScreen;
+    [SerializeField] Transform quizListHolder;
+    [SerializeField] GameObject quizButtonPrefab;
+    JsonData data;
 
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void SelectLevel()
+    {
+        HandleGetQuizes();
+        titleScreen.SetActive(false);
+        selectLevelScreen.SetActive(true);
     }
 
     public void ShowInstructions()
@@ -31,11 +43,67 @@ public class TitleScreenUI : MonoBehaviour
         titleScreen.SetActive(true);
         instructionsScreen.SetActive(false);
         settingsScreen.SetActive(false);
+        selectLevelScreen.SetActive(false);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void HandleGetQuizes()
+    {
+        StartCoroutine(GetQuizes());
+    }
+
+    IEnumerator GetQuizes()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("http://localhost:2024/quizes"))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                string json = webRequest.downloadHandler.text;
+                data = JsonMapper.ToObject(json);
+                for (int i = 0; i < data.Count; i++)
+                {
+                    GameObject quizButton = Instantiate(quizButtonPrefab, quizListHolder);
+                    quizButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = data[i]["quiz_name"].ToString() + " - " + data[i]["topic_name"].ToString() + " - " + data[i]["author"].ToString();
+                    quizButton.name = data[i]["quiz_id"].ToString();
+                    quizButton.GetComponent<Button>().onClick.AddListener(() => selectQuiz(int.Parse(quizButton.name)));
+                }
+
+            }
+        }
+    }
+
+    IEnumerator GetQuiz(int id)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("http://localhost:2024/quizes/" + id))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                string json = webRequest.downloadHandler.text;
+                quizInfo.Instance.quizJson = json;
+                StartGame();
+            }
+        }
+    }
+
+    public void selectQuiz(int index)
+    {
+        StartCoroutine(GetQuiz(index));
     }
 
 }
