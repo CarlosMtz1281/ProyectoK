b092576871670ae7173fe3a7b7bb3c0f6c2f6bb1
Marco Lucio <marcoalucio15@gmail.com>
2024-05-29 17:43:19 -0600

    Boton for creating on explorar

--- ProjectK-Web/src/app/dashboard/Admin/MisQuizes/page.tsx
+++ ProjectK-Web/src/app/dashboard/Admin/MisQuizes/page.tsx
@@ -1,56 +1,50 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import Card from "@/components/Explorar/cardQuiz"
-import '@/styles/MisQuizesAdmin.css'
+import React, { useState } from "react";
+import Card from "@/components/Explorar/cardQuiz";
+import "@/styles/MisQuizesAdmin.css";
 import { FaSearch } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
-import Select from 'react-select';
-import axios from 'axios';
-import { useEffect } from 'react';
+import Select from "react-select";
+import axios from "axios";
+import { useEffect } from "react";
+import { Fab, Tooltip } from "@mui/material";
+import { Add } from "@mui/icons-material";
+
 
 export default function Explorar() {
+  const [query, setQuery] = useState("");
+  const [selectedTema, setSelectedTema] = useState("Temas");
+  const [realData, setCardData] = useState([]);
+  const [quizRuning, setQuizRuning] = useState(true);
+  const [deleting, setDeleting] = useState(false);
+  const removeQuiz = (ID: number) => {
+    console.log("Deleting Quiz", ID);
+  };
 
-    const [query, setQuery] = useState('')
-    const [selectedTema, setSelectedTema] = useState('Temas');
-    const [realData, setCardData] = useState([])
-    const [quizRuning, setQuizRuning] = useState(true);
-    const[deleting, setDeleting] = useState(false);
-    const removeQuiz = (ID: number) => {
-        console.log("Deleting Quiz", ID);
+  const handleChangeTema = (e: any) => {
+    setSelectedTema(e);
+  };
 
-    };
+  const handleChange = (e: any) => {
+    setQuery(e.target.value);
+  };
 
-    const handleChangeTema = (e : any) => {
-        setSelectedTema(e)
-    }
-
-    const handleChange = (e : any) => {
-        setQuery(e.target.value)
-    }
-
-    // Fetch de datos
-    useEffect(() => {
-        axios
-          .get(`http://localhost:2024/quizes`)
-          .then((res) => {
-            console.log(res);
-            setCardData(res.data);
-            console.log(res.data);
-
-
-          })
-          .catch((err) => {
-            console.log(err);
-            localStorage.setItem('email', 'NOT FOUND');
-
-
-          });
-
-      },
-        []
-        );
-/*
+  // Fetch de datos
+  useEffect(() => {
+    axios
+      .get(`http://localhost:2025/quizes`)
+      .then((res) => {
+        console.log(res);
+        setCardData(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        localStorage.setItem("email", "NOT FOUND");
+      });
+  }, []);
+  /*
     const handleDelete = (id: number) => {
         axios
           .delete(`http://localhost:2024/quizes/${id}`)
@@ -62,54 +56,86 @@ export default function Explorar() {
           });
     };
 */
-    return (
-        <div className="main-explora">
-            <div className='title-container'>
-                <p className='title-explora'>
-                    Explorar Catalogo
-                </p>
-            </div>
-            <div className='flex flex-row justify-between pb-8'>
-                <div className='filter-container'>
-                    <div className='icon-container'>
-                        <IoIosSearch size={40}/>
-                    </div>
-                    <input className='searchBar'
-                        type='text'
-                        placeholder={'Busqueda por titulo de quiz ej. Calculo diferencial'}
-                        value={query}
-                        onChange={handleChange}/>
-                </div>
-                <div className='temas-button-container'>
-                    <select className="temas-button" value={selectedTema} onChange={(e) => handleChangeTema(e.target.value)}>
-                        <option value={"Temas"}>Temas</option>
-                        {
-                            (realData as { topic_name: string }[]) // Add type annotation
-                                .map(card => card.topic_name) // Get all topic_names
-                                .filter((value, index, self) => self.indexOf(value) === index) // Filter out duplicates
-                                .map((topic, index) => <option key={index} value={topic}>{topic}</option>) // Map to option elements
-                        }
-                    </select>
-                </div>
-            </div>
-            <div className='cards-container'>
-            {realData.map((card: any, index: number) => {
-                if (selectedTema === 'Temas' || card.topic_name === selectedTema) {
-                    if (card.quiz_name?.toLowerCase().includes(query.toLowerCase())) {
-                        return <Card key={index} ID={card.quiz_id} autor={card.author} nombre={card.quiz_name} tema={card.topic_name} onDelete={()=> removeQuiz(card.quiz_id)}  openQuiz = {() => setQuizRuning(true)} mayDelete={deleting}/>;
-                    } else {
-                        return null; // Don't render the card if it doesn't match the search query
-                    }
-                } else {
-                    return null; // Don't render the card if it doesn't match the selected tema
-                }
-            })}
-            </div>
-            <div className="card-buttons">
-                {deleting && <button id="readyButton" onClick={()=> setDeleting(false)}>Listo</button>}
-
-                <button id="deleteButton" onClick={()=> setDeleting(true)}>Eliminar</button>
-            </div>
+  return (
+    <div className="main-explora">
+      <div className="title-container">
+        <p className="title-explora">Explorar Catalogo</p>
+      </div>
+      <div className="flex flex-row justify-between pb-8">
+        <div className="filter-container">
+          <div className="icon-container">
+            <IoIosSearch size={40} />
+          </div>
+          <input
+            className="searchBar"
+            type="text"
+            placeholder={"Busqueda por titulo de quiz ej. Calculo diferencial"}
+            value={query}
+            onChange={handleChange}
+          />
         </div>
-    );
+        <div className="temas-button-container">
+          <select
+            className="temas-button"
+            value={selectedTema}
+            onChange={(e) => handleChangeTema(e.target.value)}
+          >
+            <option value={"Temas"}>Temas</option>
+            {
+              (realData as { topic_name: string }[]) // Add type annotation
+                .map((card) => card.topic_name) // Get all topic_names
+                .filter((value, index, self) => self.indexOf(value) === index) // Filter out duplicates
+                .map((topic, index) => (
+                  <option key={index} value={topic}>
+                    {topic}
+                  </option>
+                )) // Map to option elements
+            }
+          </select>
+        </div>
+        <div id="crearquiz" className="ml-4">
+          <Tooltip title = "Crear quiz" placement="top">
+            <Fab color="info">
+              <Add />
+            </Fab>
+          </Tooltip>
+        </div>
+      </div>
+      <div className="cards-container">
+        {realData.map((card: any, index: number) => {
+          if (selectedTema === "Temas" || card.topic_name === selectedTema) {
+            if (card.quiz_name?.toLowerCase().includes(query.toLowerCase())) {
+              return (
+                <Card
+                  key={index}
+                  ID={card.quiz_id}
+                  autor={card.author}
+                  nombre={card.quiz_name}
+                  tema={card.topic_name}
+                  onDelete={() => removeQuiz(card.quiz_id)}
+                  openQuiz={() => setQuizRuning(true)}
+                  mayDelete={deleting}
+                />
+              );
+            } else {
+              return null; // Don't render the card if it doesn't match the search query
+            }
+          } else {
+            return null; // Don't render the card if it doesn't match the selected tema
+          }
+        })}
+      </div>
+      <div className="card-buttons">
+        {deleting && (
+          <button id="readyButton" onClick={() => setDeleting(false)}>
+            Listo
+          </button>
+        )}
+
+        <button id="deleteButton" onClick={() => setDeleting(true)}>
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
 }
